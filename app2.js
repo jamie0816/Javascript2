fc5b6cf2c5827c33bd2b2b29c5524034158d5c2e
jamie0816 <jamie020816@gmail.com>
2015-09-23 16:05:24 -1000

    javascript2
    cookie

---
+++ app2.js
@@ -0,0 +1,15 @@
+var Uscore = 0;
+
+var highscore =10;
+
+var press = function() {
+	Uscore++;
+	alert("Score: " +Uscore);
+	/*
+	document.getElementById('score').innerHTML =
+	if(Uscore >highscore) {
+		document.getElementById('hiscore).inner
+		return;
+
+	}*/
+};
